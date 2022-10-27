c1a50e0330b27b68a13bf5c31e49bcf3230d3c6a
Numberedlemon <jamesathunt@hotmail.com>
2022-10-28 00:23:58 +0100

    Big Update
    Added generated GeoJSON data,, testing now..

--- Assignment 1/scripts/d3-map.js
+++ Assignment 1/scripts/d3-map.js
@@ -1,2 +1,16 @@
 // Javascript file for assignment 1 handling the drawing of the map.
 
+function d3Draw(data) {
+    d3.json("GeoJSon data/UK-Map.json", function (error, data) {
+    if (error) {
+        console.error(error);
+    }
+    console.log("UK-Map.json");
+})};
+
+function svgDraw() {
+    d3.select("body").append("svg").attr("width", 500).attr("height", 300);
+}
+
+
+window.onload = svgDraw();
