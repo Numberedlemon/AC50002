aa68a558f74cfbcbc3f89e3a6b631f29d1fc7c45
Numberedlemon <jamesathunt@hotmail.com>
2022-10-28 13:06:25 +0100

    Major Update
    Complete rewrite of d3-map.html, d3-map.js.
    
    Regenerated TopoJSON file and created mapuk-v1.json.
    
    d3-map.html now produces an outlined map the United Kingdom and Northern Ireland

--- Assignment 1/scripts/d3-map.js
+++ Assignment 1/scripts/d3-map.js
@@ -1,16 +1,32 @@
 // Javascript file for assignment 1 handling the drawing of the map.
 
-function d3Draw(data) {
-    d3.json("GeoJSon data/UK-Map.json", function (error, data) {
-    if (error) {
-        console.error(error);
-    }
-    console.log("UK-Map.json");
-})};
+const width = 900;
+const height = 600;
 
-function svgDraw() {
-    d3.select("body").append("svg").attr("width", 500).attr("height", 300);
-}
+const mapsvg = d3
+	.select("body")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height);
 
+const projection = d3
+	.geoMercator()
+	.scale(1200)
+	.translate([width / 1.5, (6 * height) / 2]);
 
-window.onload = svgDraw();
+const path = d3.geoPath(projection);
+
+const g = mapsvg.append("g");
+
+d3.json("../GeoJSON data/mapuk-v1.json").then((data) => {
+	const countries = topojson.feature(data, data.objects.mapuk);
+
+	g.selectAll("path")
+		.data(countries.features)
+		.enter()
+		.append("path")
+		.attr("class", "country")
+		.attr("d", path);
+});
+
+window.onload;
