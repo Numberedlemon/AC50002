d86404f5bf88deaf461f575cc97afd68ab0bd741
Numberedlemon <jamesathunt@hotmail.com>
2022-10-31 17:30:30 +0000

    Updates

--- Assignment 1/scripts/d3-map.js
+++ Assignment 1/scripts/d3-map.js
@@ -1,8 +1,8 @@
-// Javascript file for assignment 1 handling the drawing of the map.
+// Javascript file for assignment 1 handling the drawing of the map and plotting of the data
 
 function d3Draw() {
-const width = 500;
-const height = 500;
+const width = 600;
+const height = 600;
 
 const mapsvg = d3
 	.select("body")
@@ -12,8 +12,14 @@ const mapsvg = d3
 
 const projection = d3
 	.geoMercator()
-	.scale(1000)
-	.translate([width / 2, (5.5 * height) / 2]);
+	.center([0,55.4])
+	.scale(1300)
+	.translate([width / 2, (0.8 * height) / 2]);
+
+var tooltip = d3.select("body")
+				.append("div")
+				.attr("class", "tooltip")
+				.style("opacity",0);
 
 const path = d3.geoPath(projection);
 
@@ -28,6 +34,31 @@ d3.json("../GeoJSON data/mapuk-v1.json").then((data) => {
 		.append("path")
 		.attr("class", "country")
 		.attr("d", path);
+});
+
+d3.json("http://34.78.46.186/Circles/Towns/10").then((data) => {
+
+	g.selectAll("svg")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("class", function(d) {
+			return d.Town;
+		})
+		.attr("cx", function(d) {
+			return projection([d.lng,d.lat])[0];
+		})
+		.attr("cy", function(d) {
+			return projection([d.lng,d.lat])[1];
+		})
+		.attr("r", function(d) {
+			return 0.00005 * d.Population;
+		})
+		.append("svg:title")
+		.text(function(d) {
+			var townData = d.Town + "\nPopulation: " + d.Population + "\nCounty: " + d.County;
+			return townData;
+		})
 });
 }
 window.onload = d3Draw;
--- Assignment 1/scripts/loadData.js
+++
@@ -1,5 +0,0 @@
-const data = d3.json("http://34.78.46.186/Circles/Towns/50")
-               .then(data => {
-                d3.select("body").data(data)
-               })
-               .catch(error);
